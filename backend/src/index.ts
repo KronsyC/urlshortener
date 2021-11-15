d9985db8752f993277259c5a4571154cba8ac18f
KronsyC <87078913+KronsyC@users.noreply.github.com>
2021-11-15 17:55:42 +0000

    cheeky test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ import cookie, { FastifyCookieOptions } from "fastify-cookie"
 import Link from "./models/link"
 import cors from "fastify-cors"
 import fstatic from "fastify-static"
-import path from "path";
+import path from "path"
 
 const urlRegex = new RegExp("(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})")
 const PORT = process.env.PORT || 3000
