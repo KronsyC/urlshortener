2db479ba7ca6bfccbf05ef5c5e7e7d61ca83666c
KronsyC <87078913+KronsyC@users.noreply.github.com>
2021-11-17 16:07:09 +0000

    my hate for regex is immeasurable

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,7 @@ import fstatic from "fastify-static"
 import path from "path"
 
 
-const urlRegex = new RegExp("(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})")
+const urlRegex = new RegExp("((https?:\/\/)?(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})")
 
 
 
